4a1b6b42f0b0b365feb6edd86948742b586eedd1
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2024-12-12 22:42:34 +0500

    Create Figma.tsx

---
+++ Figma.tsx
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Figma</title>
+    <!-- You might want to include a link to your CSS here (if it's external) -->
+</head>
+
+<body>
+    <div class="bg-[#F6F6F6] border border-red-600 w-[268px] h-[432px] rounded-lg">
+        <nav class="w-full bg-slate-600">
+            <ul class="flex bg-red-300 p-5 text-white items-center justify-center gap-5">
+                <li><a href="">Home</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Gallery</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+        </nav>
+    
+        <div class="flex justify-center">
+            <img src="/iphone14.png" alt="Product One" width="200" height="200" />
+        </div>
+        
+        <h3 class="text-center pt-4 font-medium text-base">HAVIT HV-G92 Gamepad</h3>
+        
+        <div class="text-center">
+            <span>$120</span>
+        </div>
+    </div>
+</body>
+
+</html>
